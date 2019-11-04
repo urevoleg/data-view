455750ce3ba13d40073ce851b70bf9884c24d795
urevoleg <urevolegg@gmail.com>
2019-11-04 22:09:13 +0300

    add import seaborn

--- data_view/__init__.py
+++ data_view/__init__.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from IPython.display import display, HTML
 
 
--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
-     install_requires=['pandas', 'numpy', 'matplotlib', 'ipython'],
+     install_requires=['pandas', 'numpy', 'matplotlib', 'ipython', 'seaborn'],
  )
